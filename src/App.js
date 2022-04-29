aa0df312d5793a4aad7a58ae7762f28bfb9af772
tecnicoaflc <95890860+tecnicoaflc@users.noreply.github.com>
2022-04-29 11:16:09 +0200

    fggf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+ import logo from './logo.svg';
 import './App.css';
+import Movie from './movie';
+import PageWrapper from './PageWrapper';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+      
+      <Movie title="Oblivion (2012)" rate="10" runtime="2h21’" director="Joss Whedon" stars="Robert Downey Jr.,Chris Evans,Chris Hemsworth" image="images/uploads/mv1.jpg">
+        Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous <b>Loki</b> and his alien army from enslaving humanity...
+      </Movie>
+
+
+      <Movie title="into the wild (2012)" rate="7.8" runtime="2h21’" director="Joss Whedon" stars="Robert Downey Jr.,Chris Evans,Chris Hemsworth" image="images/uploads/mv2.jpg">
+        As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat...
+      </Movie>
+
+    </PageWrapper>
   );
 }
 
